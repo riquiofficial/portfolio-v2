1dcbbe68652c5770e37aa8178d9b33251655e814
Richard Quigley <richard@rqwebdev.com>
2023-01-09 21:59:37 +0000

    [ADD] dark/light theme button

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 import { ref } from 'vue'
 
 const elevation = ref(2)
+const theme = ref('dark')
+
 
 const raiseElevation = () => {
   elevation.value = 5
@@ -13,10 +15,22 @@ const reduceElevation = () => {
   console.log(elevation)
 }
 
+const changeTheme = () => {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+
 </script>
 
 <template>
-  <v-app>
-    <v-app-bar @mouseleave="reduceElevation" @mouseenter="raiseElevation" :elevation="elevation" title="Navigation" />
+  <v-app :theme=theme>
+    <v-app-bar @mouseleave="reduceElevation" @mouseenter="raiseElevation" :elevation="elevation" title="Navigation" >
+    <v-navigation-drawer>Menu stuff</v-navigation-drawer>
+
+    <v-btn
+        @click="changeTheme"
+      >Toggle Theme</v-btn>
+    
+    </v-app-bar>
+
   </v-app>
 </template>
