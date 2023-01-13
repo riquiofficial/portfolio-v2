6225bd7cdbbbd89da1fb2abf1dd0954144778ecc
Richard Quigley <richard@rqwebdev.com>
2023-01-13 11:55:19 +0000

    [ADD] Prettier formatting setup

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,46 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { mdiMenu, mdiMoonWaningCrescent, mdiWhiteBalanceSunny  } from '@mdi/js'
+import { ref } from "vue";
+import { mdiMenu, mdiMoonWaningCrescent, mdiWhiteBalanceSunny } from "@mdi/js";
 
-
-const elevation = ref(2)
-const theme = ref('dark')
-const isDarkTheme = () => theme.value === "light"
+const elevation = ref(2);
+const theme = ref("dark");
+const isDarkTheme = () => theme.value === "light";
 
 const raiseElevation = () => {
-  elevation.value = 5
-}
+  elevation.value = 5;
+};
 
 const reduceElevation = () => {
-  elevation.value = 1
-}
+  elevation.value = 1;
+};
 
 const changeTheme = () => {
-  theme.value = isDarkTheme() ? 'dark' : 'light'
-  console.log(theme.value)
-}
-
+  theme.value = isDarkTheme() ? "dark" : "light";
+  console.log(theme.value);
+};
 </script>
 
 <template>
-  <v-app :theme=theme>
-    <v-app-bar @mouseleave="reduceElevation" @mouseenter="raiseElevation" :elevation="elevation" title="Navigation" >
-      <v-app-bar-nav-icon ><v-icon :icon="mdiMenu" /></v-app-bar-nav-icon>
+  <v-app :theme="theme">
+    <v-app-bar
+      @mouseleave="reduceElevation"
+      @mouseenter="raiseElevation"
+      :elevation="elevation"
+      title="RQ Web Development"
+    >
+      <v-app-bar-nav-icon>
+        <v-icon :icon="mdiMenu" />
+      </v-app-bar-nav-icon>
 
       <v-btn @click="changeTheme()">
-        <v-icon :icon="isDarkTheme() ? mdiMoonWaningCrescent : mdiWhiteBalanceSunny" />
+        <v-icon
+          :icon="isDarkTheme() ? mdiMoonWaningCrescent : mdiWhiteBalanceSunny"
+        />
       </v-btn>
-      
-      
     </v-app-bar>
+
     <v-main>
       <v-container>Content area</v-container>
-      
-
     </v-main>
   </v-app>
 </template>
